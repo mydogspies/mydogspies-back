c5379b45f517faa60a65800614aea0f42c4d7000
mydogspies <mydogspies@zoho.com>
2021-07-02 17:37:09 +0200

    Fixed cors error

--- server.js
+++ server.js
@@ -28,7 +28,11 @@ const app = express();
 //     };
 //     app.use(cors(corsOptions));
 // }
-app.use(cors());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 // define body parser
 app.use(express.json());
