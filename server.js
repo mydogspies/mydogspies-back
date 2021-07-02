1b35a62396d1e27bd0b1e23518ce7400d3889bc1
mydogspies <mydogspies@zoho.com>
2021-07-02 16:18:45 +0200

    Fixed cors error

--- server.js
+++ server.js
@@ -21,14 +21,16 @@ const status = require('./routes/status');
 const app = express();
 
 // cors
-let corsOptions = null;
-if (process.env.NODE_ENV === 'production') {
-    const corsOptions = {
-        origin: 'http://10.110.0.3',
-        optionsSuccessStatus: 200
-    };
-}
-app.use(cors(corsOptions));
+// let corsOptions = null;
+// if (process.env.NODE_ENV === 'production') {
+//     const corsOptions = {
+//         origin: 'http://10.110.0.3',
+//         optionsSuccessStatus: 200
+//     };
+// }
+// app.use(cors(corsOptions));
+
+app.use(cors());
 
 // define body parser
 app.use(express.json());
