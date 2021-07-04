7faf20ab2cd5168594856067f2080eb6b2477387
mydogspies <mydogspies@zoho.com>
2021-07-04 11:01:39 +0200

    Added Okta for endpoint authentication

--- routes/status.js
+++ routes/status.js
@@ -10,7 +10,7 @@ const router = express.Router();
 
 router
     .route('/')
-    .get(getServerStatus, authenticationRequired)
+    .get(getServerStatus)
     .put(updateServerStatus, authenticationRequired);
 
 module.exports = router;
